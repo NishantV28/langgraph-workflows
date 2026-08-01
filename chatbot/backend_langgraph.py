ab3c8ef49c07e7e0812b9e99179bec6d13df61bc
NishantV28 <varshneynishant686@gmail.com>
2026-08-02 00:25:44 +0530

    fix: convert chatbot from submodule to regular folder

---
+++ chatbot/backend_langgraph.py
@@ -0,0 +1,29 @@
+from langgraph.graph import StateGraph,START, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated 
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph.message import add_messages  
+from langgraph.checkpoint.memory import InMemorySaver
+
+load_dotenv()  
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+def chat_node(State: ChatState):
+    messages =State['messages']
+    response = model.invoke(messages)
+    return {'messages': [response]} 
+
+
+checkpointer = InMemorySaver() 
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node",chat_node)
+
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer) 
